82dae9c2df4d8b4c1d2e7a9795ae11e5382e39d0
master_cs_se <jose@jortizsd.com>
2016-10-26 10:53:33 -0700

    Add header documentation for main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,7 @@
 /**
- * Created by master_cs_se on 10/26/16.
+ * Created by Jose Ortiz on 10/26/16.
+ * File : main.js
+ * Description: This file will init all the starting app points
+ *              such as database....etc. Also, it will contains
+ *              the global variables for this app.
  */

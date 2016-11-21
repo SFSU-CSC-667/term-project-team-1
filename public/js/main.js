21d7c24785f14eca2754756cce6c3fa72f8bdd4b
leo <lrod408@yahoo.com>
2016-11-21 12:10:15 -0800

    fixed sort

--- public/js/main.js
+++ public/js/main.js
@@ -53,7 +53,7 @@ function populateTable() {
 function sortByKey(array, key) {
     return array.sort(function(a, b) {
         var x = a[key]; var y = b[key];
-        return ((x > y) ? -1 : ((x > y) ? 1 : 0));
+        return ((x > y) ? -1 : ((x < y) ? 1 : 0));
     });
 }
 

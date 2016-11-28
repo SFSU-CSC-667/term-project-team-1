26e0b9774dfd9f6ce3849c06b8efec329a558f3f
master_cs_se <jose@jortizsd.com>
2016-11-28 11:53:56 -0800

    Added redirect to main page when login error ocurrs.

---
+++ routes/testgameplay.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+
+router.get('/', function(req, res, next) {
+    //Do whatever...
+    res.render('');
+});/**
+ * Created by master_cs_se on 11/28/16.
+ */

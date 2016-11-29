52ba46ae4a5cca5d7d675592ad47ca3a54730282
master_cs_se <jose@jortizsd.com>
2016-11-29 15:32:27 -0800

    Changed path in main.js

--- public/js/main.js
+++ public/js/main.js
@@ -11,87 +11,45 @@ var userListData = [];
 $(document).ready(function() {
 
     // Populate the user table on initial page load
-    loadLeadersList();
-    loadGamesList();
-
+    populateTable();
+    loadLobbies();
+    var g = getGame(3);
+    console.log(g.name);
 
 });
 
 // Functions =============================================================
 
 // Fill table with data
-function loadLeadersList() {
+function populateTable() {
 
     // Empty content string
     var tableContent = '';
 
     // jQuery AJAX call for JSON
-    $.getJSON('https://csc667-team1-tetris.herokuapp.com/dbapi/users', function (data) {
+    $.getJSON( '/dbAPI/users', function( data ) {
         if (data['status'] == 'success') {
 
             // For each item in our JSON, add a table row and cells to the content strin
             var users = sortByKey(data['users'], 'score')
-            for (var i = 0; i < users.length; i++) {
+            for (var i = 0; i<users.length; i++) {
 
                 var user = users[i];
 
                 tableContent += '<tr>';
                 tableContent += '<td>' + (i + 1) + '</td>';
-                tableContent += '<td>' + user.name + '</td>';
-                tableContent += '<td>' + user.email + '</td>';
+                tableContent += '<td>' +  user.name + '</td>';
+                tableContent += '<td>' +  user.email + '</td>';
                 tableContent += '<td>' + user.score + '</td>';
                 tableContent += '</tr>';
 
 
                 // Inject the whole content string into our existing HTML table
-                $('#wrapper table tbody').html(tableContent);
-            }
-        }
-
-    });
-}
-
-
-/* Loads the list of lobbies in lobbyRoom page */
-
-// Fill table with data
-function loadGamesList() {
-
-    // Empty content string
-    var tableContent = '';
-
-    // jQuery AJAX call for JSON
-    $.getJSON('https://csc667-team1-tetris.herokuapp.com/dbapi/games', function (data) {
-        if (data['status'] == 'success') {
-
-            // For each item in our JSON, add a table row and cells to the content strin
-            var games = data['games'];
-            var isActive = 'game is full';
-            for (var i = 0; i < games.length; i++) {
-                var game = games[i];
-                var join = "join game";
-                if (game.isfull == true) {
-                    join = "This game is full";
-                }
-
-                tableContent += '<tr>';
-                tableContent += '<td>' + game.name + '</td>';
-                tableContent += '<td>' + game.gamename + '</td>';
-                tableContent += '<td>' + '<a href="/dbApi/games/' + game.id + '">' + join + '</a></td>';
-                tableContent += '<td>' + game.totalscore + '</td>';
-
-                tableContent += '</tr>';
-
-
-                // Inject the whole content string into our existing HTML table
-                $('#gamesList table tbody').html(tableContent);
+                $('#userList table tbody').html(tableContent);
             }
         }
     });
-
-}
-
-
+};
 
 
 /* Sorts json item in descending order by key */

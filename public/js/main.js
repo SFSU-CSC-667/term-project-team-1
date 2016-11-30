6c10a5cf2d2c71fb30b75cb1832fb8b6c8fc2af6
master_cs_se <jose@jortizsd.com>
2016-11-30 11:55:17 -0800

    Back to original configuration in main file

--- public/js/main.js
+++ public/js/main.js
@@ -52,6 +52,38 @@ function loadLeadersBoard() {
     });
 }
 
+function loadGamesList() {
+// jQuery AJAX call for JSON
+    // Empty content string
+    var tableContent = '';
+    $.getJSON('/dbapi/games', function (data) {
+        if (data['status'] == 'success') {
+
+            // For each item in our JSON, add a table row and cells to the content strin
+            var games = sortByKey(data['games']['id'])
+            for (var i = 0; i < games.length; i++) {
+
+                var game = games[i];
+                var join = "Join to this game";
+                if (game.isfull == true) {
+                    join = "This game is already in progress";
+                    document.getElementById('joinlink').setAttribute('href', '');
+                }
+                tableContent += '<tr>';
+                tableContent += '<td>' + game.name + '</td>';
+                tableContent += '<td>' + game.gamename + '</td>';
+                tableContent += '<td>' + game.totalscore + '</td>';
+                tableContent += '<td><a id="joinlink" href="/gameplay">' + join + '</a></td>';
+                tableContent += '</tr>';
+
+
+                // Inject the whole content string into our existing HTML table
+                $('#gamesList table tbody').html(tableContent);
+            }
+        }
+    });
+}
+
 /* Sorts json item in descending order by key */
 function sortByKey(array, key) {
     return array.sort(function(a, b) {

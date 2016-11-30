342d4c6406fef6ad9151b1a5e7ecf3a5f3aff296
master_cs_se <jose@jortizsd.com>
2016-11-30 10:15:23 -0800

    Fixed Game List

--- public/js/main.js
+++ public/js/main.js
@@ -11,7 +11,8 @@ var userListData = [];
 $(document).ready(function() {
 
     // Populate the user table on initial page load
-    populateTable();
+    loadLeadersBoard();
+    loadGamesList();
     
 
 
@@ -20,7 +21,7 @@ $(document).ready(function() {
 // Functions =============================================================
 
 // Fill table with data
-function populateTable() {
+function loadLeadersBoard() {
 
     // Empty content string
     var tableContent = '';
@@ -41,6 +42,45 @@ function populateTable() {
                 tableContent += '<td>' +  user.name + '</td>';
                 tableContent += '<td>' +  user.email + '</td>';
                 tableContent += '<td>' + user.score + '</td>';
+                tableContent += '</tr>';
+
+
+                // Inject the whole content string into our existing HTML table
+                $('#userList table tbody').html(tableContent);
+            }
+        }
+    });
+}
+
+// Functions =============================================================
+
+// Fill table with data
+function loadGamesList() {
+
+    // Empty content string
+    var tableContent = '';
+
+
+    // jQuery AJAX call for JSON
+    $.getJSON( '/dbAPI/games', function( data ) {
+        if (data['status'] == 'success') {
+
+            // For each item in our JSON, add a table row and cells to the content strin
+            var games = sortByKey(data['games'], 'gamescore');
+            for (var i = 0; i<games.length; i++) {
+
+                var game = games[i];
+                var joinGame = "Join";
+                if (game.isfull == true)
+                {
+                    joinGame = "Game is full";
+                }
+
+                tableContent += '<tr>';
+                tableContent += '<td>' +  game.name + '</td>';
+                tableContent += '<td>' +  game.gamename + '</td>';
+                tableContent += '<td>' +  joinGame + '</td>';
+                tableContent += '<td>' +  game.totalscore + '</td>';
                 tableContent += '</tr>';
 
 

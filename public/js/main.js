42d4c3ba325ae5a729772c11d90ecc912ae7a3f7
master_cs_se <jose@jortizsd.com>
2016-11-30 11:29:01 -0800

    Added link to join game in list of games

--- public/js/main.js
+++ public/js/main.js
@@ -69,12 +69,17 @@ function loadGamesList() {
             for (var i = 0; i<games.length; i++) {
 
                 var game = games[i];
-
+                var join = "Join to this game";
+                if (game.isfull == true)
+                {
+                    join = "This game is already in progress";
+                    document.getElementById('joinlink').setAttribute('href', '');
+                }
                 tableContent += '<tr>';
                 tableContent += '<td>' +  game.name + '</td>';
                 tableContent += '<td>' +  game.gamename + '</td>';
-                tableContent += '<td>' +  game.isfull + '</td>';
-                tableContent += '<td>' +  game.totalscore + '</td>';
+                tableContent += '<td><' +  join + '</td>';
+                tableContent += '<td><a id="joinlink" href="/testgameplay">' +  game.totalscore + '</a></td>';
                 tableContent += '</tr>';
 
 

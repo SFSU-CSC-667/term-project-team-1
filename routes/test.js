19fd52f552e9427e7860c731199965f8c341e11b
master_cs_se <jose@jortizsd.com>
2016-12-03 20:16:55 -0800

    Front end for login error implemented

--- routes/test.js
+++ routes/test.js
@@ -4,13 +4,8 @@ var router = express.Router();
 /* GET users listing. */
 
 router.get('/', function(req, res, next) {
-    //Do whatever...
-    var error = "error";
-    if (req.parameters.error == 1)
-    {
-        error = 'Your email or password are incorrect';
-    }
-    res.render('test', {h3: error});
+
+    res.render('test', {error: 'This is a test error'});
 });
 
 

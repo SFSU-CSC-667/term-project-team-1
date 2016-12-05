03be56082a20bbad675dbfa2f9d4e6a54b8f9fcc
master_cs_se <jose@jortizsd.com>
2016-12-04 21:26:14 -0800

    Added skeleton multiplayer functions to multiplayer

--- public/js/multiplayer.js
+++ public/js/multiplayer.js
@@ -8,10 +8,44 @@ $(document).ready(function()
 
     initMultiplayerSocket();
 
+
 });
 
 function initMultiplayerSocket ()
 {
     var multiplayerSocket = io();
     multiplayerSocket.emit("multiplayer", "Jose");
+}
+
+function getParameterByName(name, url) {
+    if (!url) {
+        url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function getPlayer1Info ()
+{
+
+}
+
+function getPlayer2Info ()
+{
+
+}
+
+function updatePlayerScore (playerid)
+{
+
+}
+
+
+function updateGameScore (gameid)
+{
+
 }

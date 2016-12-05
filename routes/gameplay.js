594565ce84fbc40e5e6ebeb22a980a42d87d71aa
master_cs_se <jose@jortizsd.com>
2016-12-04 21:06:42 -0800

    Added skeleton logic for multiplayer

--- routes/gameplay.js
+++ routes/gameplay.js
@@ -3,17 +3,35 @@ var router = express.Router();
 var sessions = require('../server_classes/config/sessions');
 
 
+
+
 /* GET users listing. */
 
 router.get('/', function(req, res, next) {
     //Do whatever...
     if (sessions.USER_SESSION == -1) {
+
+
+
         res.render('test');
+
 
     }
     else
     {
+
+        if (sessions.JOINED_USER_SESSION == sessions.USER_SESSION)
+        {
+            // user is playing against itself
+        }
+        else if (sessions.JOINED_USER_SESSION > -1)
+        {
+            // another user entered to the game
+            // call socket io multiplayer logic from here
+        }
         res.render('gameplay');
+
+
 
     }
 });

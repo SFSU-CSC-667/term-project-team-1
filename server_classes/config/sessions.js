e53aa3e968ec251cae3933a61eca0b9d2a8a61a7
master_cs_se <jose@jortizsd.com>
2016-12-04 16:16:12 -0800

    Changed session expiring from 1 minute to 30 minutes

--- server_classes/config/sessions.js
+++ server_classes/config/sessions.js
@@ -19,7 +19,7 @@ var app_sessions = {
     'JOINED_USER_SESSION': -1, // session for user joined to a game.
     'GAME_SESSION': -1, // session for a game
     'EXPIRED_SESSION': 1, // determine if the session was expired
-    'SESSION_EXPIRING_MAX_TIME': 60*1000 // must be in miliseconds
+    'SESSION_EXPIRING_MAX_TIME': 60*1000*30 // must be in miliseconds
 
 }
 

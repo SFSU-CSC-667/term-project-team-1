57522413df92c88cee9f909446b5c4145581f586
Adison <moo5e@mail.sfsu.edu>
2016-12-05 07:57:37 -0800

    Fixing minor error

--- server_classes/database/models/users.js
+++ server_classes/database/models/users.js
@@ -83,7 +83,7 @@ function validateUser (req, res, next) {
                     session.EXPIRED_SESSION = 1; // expire the session
                     session.GAME_SESSION = -1; // expire game session.
                 }, session.SESSION_EXPIRING_MAX_TIME);
-                res.redirect('/game');
+                res.redirect('/create_game');
             }
 
         })

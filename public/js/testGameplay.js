a8fd710147ef5e375d3c66a97be53ba5ed08b79d
nparatka <nparatka@mail.sfsu.edu>
2016-12-08 10:00:57 -0800

    send updated final score

--- public/js/testGameplay.js
+++ public/js/testGameplay.js
@@ -106,8 +106,8 @@ function end(){
   show('start');
     setdisplayedScore();
   playing = false;
-  var finalscore=document.getElementById('score').innerHTML;
-  updateScore(finalscore);
+  //var finalscore=document.getElementById('score').innerHTML;
+  updateScore(currentscore);
 }
 
 function reset(){
